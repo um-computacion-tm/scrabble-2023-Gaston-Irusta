092a0442431af0a3f96ce78c072bec23aa6a99a7
Gaston-Irusta <g.irusta@alumno.um.edu.ar>
2023-09-13 08:30:37 -0300

    main.py update

--- main.py
+++ main.py
@@ -21,18 +21,18 @@ def main():
     scrabble_game.next_turn()
     print(f"Turno del jugardor {scrabble_game.current_player}")
     option = int(input())
-    if option == 1:
-        word = input("Ingrese palabra.")
-        location_x = input("Ingrese posicion X.")
-        location_y = input("Ingrese pocision Y")
-        location = (location_x,location_y)
-        orientation = input("Ingrese orientación (V/H)")
-        scrabble_game.validate_word(word, location, orientation, player_tiles=scrabble_game.current_player.tiles)
-    if option == 2:
-        print('Fichas del jugador:\n',scrabble_game.current_player.tiles)
-    if option == 3:
-        tile = str(input('¿Qué ficha quiere intercambiar?\n'))
-        tile = tile.upper()
-        '''Buscar esa letra en las fichas del jugador y asignarla a tile para meterla en la bolsa.???'''
-        scrabble_game.bag_tiles.put(tile)
-        scrabble_game.bag_tiles.take(1)
+    # if option == 1:
+    #     word = input("Ingrese palabra.")
+    #     location_x = input("Ingrese posicion X.")
+    #     location_y = input("Ingrese pocision Y")
+    #     location = (location_x,location_y)
+    #     orientation = input("Ingrese orientación (V/H)")
+    #     scrabble_game.validate_word(word, location, orientation, player_tiles=scrabble_game.current_player.tiles)
+    # if option == 2:
+    #     print('Fichas del jugador:\n',scrabble_game.current_player.tiles)
+    # if option == 3:
+    #     tile = str(input('¿Qué ficha quiere intercambiar?\n'))
+    #     tile = tile.upper()
+    #     '''Buscar esa letra en las fichas del jugador y asignarla a tile para meterla en la bolsa.???'''
+    #     scrabble_game.bag_tiles.put(tile)
+    #     scrabble_game.bag_tiles.take(1)
