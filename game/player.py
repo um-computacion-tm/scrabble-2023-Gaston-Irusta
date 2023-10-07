ca5d5d5617965f5dfd6fee5a9a683d410d058150
Gaston-Irusta <g.irusta@alumno.um.edu.ar>
2023-10-07 18:06:12 -0300

    Finished validate and put word with tests

--- game/player.py
+++ game/player.py
@@ -1,5 +1,4 @@
 from game.models import Tile
-from calculate_word_value import calculate_word_value
 
 class Player:
     def __init__(self, id:int, tiles: list[Tile],nickname = None,score = 0):
@@ -8,5 +7,3 @@ class Player:
         self.nickname = nickname
         self.score = score
     
-    def upgrd_score(self,word_tiles):
-        self.score = (self.score + calculate_word_value(word_tiles))
